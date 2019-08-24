b5745489282477c2a49a066c3d08a8b9920dd0a3
kenwang92 <51316165+kenwang92@users.noreply.github.com>
2019-08-24 18:01:47 +0800

    Update PTTget.py

--- PTTget.py
+++ PTTget.py
@@ -3,6 +3,12 @@ import time
 from bs4 import BeautifulSoup
 import re
 
+class color:
+    numless10 = '\033[32m'
+    numuch10 = '\033[33m'
+    numboom = '\033[31m'
+    numx = '\033[33m'
+    numrst = '\033[0m'
 print('PTT開爬')
 
 r = requests.Session()
@@ -22,11 +28,28 @@ for soup in soup.find_all('div',{"class":"r-ent"}):
     if soup.find('div','author').string == '-':#是否被刪文
         pass
     else:
-        if soup.find('span'):#判斷有沒有得抓推文數
-            print(soup.find('span').string, soup.find('a').string, 'https://www.ptt.cc', end='')
-            print(soup.find('a')['href'])#有的話抓推文數,標題和連結
+        if soup.find('span'):#判斷有沒有得抓推文數,有的話抓推文數,標題和連結
+            spanstr = soup.find('span').string
+            if(spanstr.isdigit()):
+                if (int(spanstr) < 10):#推文數小於10用綠色,大於用黃色
+                    print(' ' + color.numless10 + soup.find('span').string, end=' ')
+                    print(color.numrst + soup.find('a').string, 'https://www.ptt.cc', end='')
+                    print(soup.find('a')['href'])
+                else:
+                    print(color.numuch10 + soup.find('span').string, end=' ')
+                    print(color.numrst + soup.find('a').string, 'https://www.ptt.cc', end='')
+                    print(soup.find('a')['href'])
+            else:#爆文或X...要紅色或灰色
+                if (spanstr == '爆'):#推文數為爆用紅色,x..用灰色
+                    print(color.numboom + soup.find('span').string, end=' ')
+                    print(color.numrst + soup.find('a').string, 'https://www.ptt.cc', end='')
+                    print(soup.find('a')['href'])
+                else:
+                    print(color.numx + soup.find('span').string, end=' ')
+                    print(color.numrst + soup.find('a').string, 'https://www.ptt.cc', end='')
+                    print(soup.find('a')['href'])
         else:
-            print(' ',soup.find('a').string, 'https://www.ptt.cc', end='')
+            print('  ',soup.find('a').string, 'https://www.ptt.cc', end='')
             print(soup.find('a')['href'])
 
 while(1):
@@ -38,12 +61,29 @@ while(1):
         if c.find('div','author').string == '-':#是否被刪文
             pass
         else:
-            if c.find('span'):#判斷有沒有得抓推文數
-                print(c.find('span').string, c.find('a').string, 'https://www.ptt.cc', end='')
-                print(c.find('a')['href'])#有的話抓推文數,標題和連結
+            if c.find('span'):#判斷有沒有得抓推文數有的話抓推文數,標題和連結
+                spanstr2 = c.find('span').string
+                if(spanstr2.isdigit()):
+                    if (int(c.find('span').string) < 10):#推文數小於10用綠色,大於用黃色
+                        print(' ' + color.numless10 + c.find('span').string, end=' ')
+                        print(color.numrst + c.find('a').string, 'https://www.ptt.cc', end='')
+                        print(c.find('a')['href'])#有的話抓推文數,標題和連結
+                    else:
+                        print(color.numuch10 + c.find('span').string, end=' ')
+                        print(color.numrst + c.find('a').string, 'https://www.ptt.cc', end='')
+                        print(c.find('a')['href'])#有的話抓推文數,標題和連結
+                else:#爆文或X...要紅色或灰色
+                    if (soup.find('span').string == '爆'):#推文數為爆用紅色,x..用灰色
+                        print(color.numboom + c.find('span').string, end=' ')
+                        print(color.numrst + c.find('a').string, 'https://www.ptt.cc', end='')
+                        print(c.find('a')['href'])#有的話抓推文數,標題和連結
+                    else:
+                        print(color.numx + c.find('span').string, end=' ')
+                        print(color.numrst + c.find('a').string, 'https://www.ptt.cc', end='')
+                        print(c.find('a')['href'])#有的話抓推文數,標題和連結
             else:
-                print(c.find('a').string, 'https://www.ptt.cc', end='')
-                print(c.find('a')['href'],end='')
+                print('  ',c.find('a').string, 'https://www.ptt.cc', end='')
+                print(c.find('a')['href'])
     #取下次要用的按紐值
     UPnum = re.findall(r'\d+',str(UP))
     UPnum = UPnum[1]
